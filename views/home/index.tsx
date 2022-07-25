182396a64cbfbae9728a297bb4040107bb740954
oke kehinde <okekehinde@gmail.com>
2022-07-25 16:32:34 +0200

    info section

--- views/home/index.tsx
+++ views/home/index.tsx
@@ -43,6 +43,68 @@ const HomeView = ()=>{
             <HiLocationMarker  className='mr-2'/> Lagos Nigeria
             </div>
         </section>   
+        <section className='text-center my-6  bg-red-100 p-6'>
+            <h1 className='text-6xl mb-2'>Apply &#38;
+             </h1>
+            <h1 className='text-6xl mb-12 text-red-700'>   
+            Join the Conversation
+             </h1>
+
+
+            <div className="container mx-auto">
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-3 ">
+    <div className="flex justify-center text-2xl brd border:dotted border-2 border-red-300 hover:bg-red-200 hover:opacity-80 p-4 cursor-pointer items-center">
+        Apply as a Speaker
+    </div>
+    <div className="flex justify-center text-2xl brd border:dotted border-2 border-red-300 hover:bg-red-200 hover:opacity-80 p-4 cursor-pointer items-center">
+        Apply as a Sponsor
+    </div>
+    <div className="flex justify-center text-2xl brd border:dotted border-2 border-red-300 hover:bg-red-200 hover:opacity-80 p-4 cursor-pointer items-center">
+        Apply as a Volunteer
+    </div>
+    <div className="flex justify-center text-2xl brd border:dotted border-2 border-red-300 hover:bg-red-200 hover:opacity-80 p-4 cursor-pointer items-center">
+        Apply as a Media Partner
+    </div>
+
+  </div>
+</div>
+        </section>
+
+        <section className='max-w-6xl m-auto my-16 ' >
+        <div className="md:grid md:grid-cols-12">
+  <div className="md:col-span-6 col-span-6">
+    <Image
+    src='/speaker-one.jpg'
+ 
+    width="100%" height="70%" layout="responsive" objectFit="cover"
+      />
+  </div>
+  <div className="col-span-6 md:pl-20 [&>p]:mb-3 p-3 md:p-0">
+    <h2 className='text-3xl font-semibold leading-10 mb-8'>Lorem ipsum dolor sit amet, consectetur adipisi.</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae maxime reprehenderit nobis, sunt quod voluptatibus odit itaque deserunt autem. Commodi consequuntur perferendis deleniti voluptate molestiae sunt exercitationem aliquid ipsam nisi, necessitatibus, modi excepturi fuga ullam odio aspernatur quisquam eos earum?</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, earum sapiente, ipsum minus explicabo animi quis laboriosam libero blanditiis vel nobis, labore totam quae ut.</p>
+  </div>
+</div>
+        </section>
+
+        <section className='max-w-6xl m-auto my-16 ' >
+        <div className="md:grid md:grid-cols-12">
+  <div className="col-span-6 md:pl-20 [&>p]:mb-3 p-3 md:p-0">
+    <h2 className='text-3xl font-semibold leading-10 mb-8'>Lorem ipsum dolor sit amet, consectetur adipisi.</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae maxime reprehenderit nobis, sunt quod voluptatibus odit itaque deserunt autem. Commodi consequuntur perferendis deleniti voluptate molestiae sunt exercitationem aliquid ipsam nisi, necessitatibus, modi excepturi fuga ullam odio aspernatur quisquam eos earum?</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, earum sapiente, ipsum minus explicabo animi quis laboriosam libero blanditiis vel nobis, labore totam quae ut.</p>
+  </div>
+  <div className="md:col-span-6 col-span-6">
+    <Image
+    src='/speaker-two.jpg'
+ 
+    width="100%" height="70%" layout="responsive" objectFit="cover"
+      />
+  </div>
+</div>
+        </section>
     </>)
 }
 
