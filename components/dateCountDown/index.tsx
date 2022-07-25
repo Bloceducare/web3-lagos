9f660b00dd9be6278e54e469d9ac5e386ec12e6c
oke kehinde <okekehinde@gmail.com>
2022-07-25 14:08:19 +0200

    misc

--- components/dateCountDown/index.tsx
+++ components/dateCountDown/index.tsx
@@ -15,7 +15,8 @@ interface Props {
 const DateCountDown = ({className=''}:Props) => {
   return (<>   
     <div className={classNames(className, ' p-3 md:-mt-8 rounded-md md:bg-white')}> 
-    <DynamicComponent dateTo="January 01, 2023 00:00:00 GMT+03:00"  style = {{
+    {/* @ts-ignore */}
+     <DynamicComponent dateTo="January 01, 2023 00:00:00 GMT+03:00"  style = {{
       border:"1px solid"
     }} />
     </div>
