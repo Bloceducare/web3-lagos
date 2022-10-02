16b7836c4a33004b2c819751758485ef4d7a95bc
Oke Kehinde <55279370+okeken@users.noreply.github.com>
2022-10-02 10:27:26 +0100

    Update data.ts

--- data.ts
+++ data.ts
@@ -1,4 +1,4 @@
 
-export const speakersImg=['/isaac.svg', '/justin.svg', '/tutu.svg', '/lanre.svg', '/trust.svg', '/jude.svg', '/mayowa.svg', '/lucky.svg', '/tim.svg']
+export const speakersImg=['/isaac.png', '/justin.png', '/tutu.png', '/lanre.png', '/trust.svg', '/jude.svg', '/mayowa.svg', '/lucky.svg', '/tim.svg']
 
 export const blurUrl = "data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRm knyJckliyjqTzSlT54b6bk+h0R//2Q=="
