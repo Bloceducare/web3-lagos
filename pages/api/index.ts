fe39cd691c99b115c24a14ac7a05547e5928519f
okeken <okekehinde@gmail.com>
2023-04-29 05:39:55 +0100

    ci updates

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,3 +1,5 @@
-export default function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ name: "John Doe" });
 }
