21af11f46740135a0b29c827966e41f9c25f78d3
superman <goodnesskolapo@gmail.com>
2024-06-12 16:45:56 +0100

    ...

--- backend/registration/models.py
+++ backend/registration/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-
 class SpeakerRegistration(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     topic = models.CharField(max_length=200)
-    bio = models.TextField()
+    bio = models.TextField(blank=True)
 
 class GeneralRegistration(models.Model):
     name = models.CharField(max_length=100)
@@ -15,26 +14,28 @@ class GeneralRegistration(models.Model):
 
 class Attendance(models.Model):
     participant = models.ForeignKey(GeneralRegistration, on_delete=models.CASCADE)
-    day = models.PositiveSmallIntegerField()
+    day = models.PositiveSmallIntegerField(default=1)
     present = models.BooleanField(default=False)
-
     class Meta:
         unique_together = ('participant', 'day')
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
-    creator = models.ForeignKey(GeneralRegistration, on_delete=models.CASCADE)  
+    creator = models.ForeignKey(GeneralRegistration, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, max_length=1000)
+    project_repository = models.URLField(blank=True)
+    live_link = models.URLField(blank=True)  
     members = models.ManyToManyField(GeneralRegistration, related_name='teams', blank=True)
 
 class HackathonRegistration(models.Model):
     participant = models.ForeignKey(GeneralRegistration, on_delete=models.CASCADE)
-    project_name = models.CharField(max_length=200)
-    project_description = models.TextField()
+    stack = models.CharField(max_length=200)
+    personal_github = models.URLField(blank=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
 class RoadToWeb3LagosRegistration(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     blockchain_experience = models.CharField(max_length=200)
-    motivation = models.TextField()
+    motivation = models.TextField(blank=True)
     status = models.BooleanField(default=False)
