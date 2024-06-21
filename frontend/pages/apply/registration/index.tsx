1a07079a33014dfe35c316456c5ff21bf8cc7750
superman <goodnesskolapo@gmail.com>
2024-06-21 15:55:45 +0100

    merge error fix

--- frontend/pages/apply/registration/index.tsx
+++ frontend/pages/apply/registration/index.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import  ApplyAsAnAttendant  from "../../../views/apply-registration/index";
-=======
-import { ApplyAsAnAttendant } from "@/views";
->>>>>>> ee153a26b62f66c228f1180b394d3af839f63919
 
 const Register = () => {
   return (
