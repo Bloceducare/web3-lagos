0294f8851618ec79464c46b62ccfd70ae276593a
Nomolos29 <86138279+Nomolos29@users.noreply.github.com>
2024-06-27 23:40:11 +0100

    add overview icon cards

--- frontend/pages/insight.tsx
+++ frontend/pages/insight.tsx
@@ -7,73 +7,94 @@ import play from "../public/play.svg";
 import barchat from "../public/bar.svg";
 import barchatStat from "../public/barchart.png";
 import { YoutubeIframe, Contact } from "@/components";
-function Insight() {
+import { DataFetcher } from "../components";
+
+const Insight: React.FC = () => {
+  type IconBoxProps = {
+    value: string;
+    title: string;
+    src: string;
+  };
+
+  const IconBox: React.FC<IconBoxProps> = ({ src, title, value }) => (
+    <div className="w-[26rem] bg-[#ffffff] flex-shrink-0">
+      <div className="flex w-full shadow-xl rounded-lg justify-between pl-10 py-8">
+        <div className="w-[35%] h-[8rem] flex justify-center items-center bg-[#ccc] rounded-full">
+          <Image src={src} alt="stats" />
+        </div>
+        <div className="w-[50%] space-y-0 m-auto">
+          <p className="text-[20px] text-[#454c70] font-[700] md:text-[48px] lg:text-[50px] text-center leading-[24.5px] md:leading-[52.8px] lg:leading-[52.8px]">
+            {value}
+          </p>
+          <p className="text-[20px] text-center text-[#23242A] font-[600] lg:text-[20px] leading-[24.5px] md:leading-[52.8px] lg:leading-[52.8px]">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
-    <div className="flex flex-col justify-center items-center w-full pt-[5.5rem]">
-      <section className="lg:max-w-screen-lg xl:max-w-screen-xl">
-        <div className="lg:px-6 md:grid md:grid-cols-2 lg:grid lg:grid-cols-2 gap-x-24 items-baseline">
-          <div>
-            <div className="bg-[#F8B135] p-6 rounded-lg">
-              <div className="flex items-center space-x-12">
-                <div>
-                  <Image src={notes} alt="stats" />
-                </div>
-                <div>
-                  <p className="text-[20px] md:text-[48px] font-[500] lg:text-[48px] leading-[24.5px] md:leading-[52.8px] lg:leading-[52.8px]">
-                    Total Registration
-                  </p>
-                  <p className="text-[20px] md:text-[48px] font-[500], lg:text-[48px] leading-[24.5px] md:leading-[52.8px] font-[700] lg:leading-[52.8px]">
-                    2332
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="mt-6">
-              <h2 className="text-[#23242A] text-[36px] leading-[39px] font-[700]">
-                Registration Breakdown -{" "}
-              </h2>
-              <ul className="text-[#23242A] text-[32px] leading-[35px]">
-                <li>23 States in Nigeria</li>
-                <li>14 African Countries</li>
-                <li>3 European Countries</li>
-                <li>3 Asian Countries</li>
-                <li>USA and UK</li>
-              </ul>
+    <div className="flex flex-col justify-center items-center w-full pt-[7.5rem] bg-[#fdfdfd]">
+      <section className="lg:px-6 lg:max-w-screen-lg xl:max-w-screen-xl">
+        <h1 className="text-[#053758] font-[700] text-[2.6rem] tracking-wider mb-[2rem]">Event Overview</h1>
+        <div className="gap-x-24 items-baseline">
+          <div className="w-full overflow-x-scroll flex flex-col justify-center">
+            <div className="flex pb-10 gap-10 w-max">
+              <DataFetcher>
+                {({ registrations }) => (
+                  <IconBox src={notes} title="Total Registration" value={registrations.length > 0 ? `${registrations.length}` : '0'} />
+                )}
+              </DataFetcher>
+
+              <DataFetcher>
+                {({ registrations }) => (
+                  <IconBox src={group} title="Total Attendance" value="2447" />
+                )}
+              </DataFetcher>
+
+              <DataFetcher>
+                {({ speakers }) => (
+                  <IconBox src={speech} title="Speakers" value={speakers.length > 0 ? `${speakers.length}` : '0'} />
+                )}
+              </DataFetcher>
+
+              <DataFetcher>
+                {({ registrations }) => (
+                  <IconBox src={play} title="Total Streams" value="3,161" />
+                )}
+              </DataFetcher>
             </div>
           </div>
 
+          <div className="mt-10">
+            <h2 className="text-[#23242A] text-[36px] leading-[39px] font-[700]">
+              Registration Breakdown
+            </h2>
+            <ul className="text-[#23242A] text-[22px] flex mt-[2rem] gap-5 leading-[35px]">
+              <li className="w-full bg-[#ffffff] shadow-xl rounded-lg px-5 py-4">23 States in Nigeria</li>
+              <li className="w-full bg-[#ffffff] shadow-xl rounded-lg px-5 py-4">14 African Countries</li>
+              <li className="w-full bg-[#ffffff] shadow-xl rounded-lg px-5 py-4">3 European Countries</li>
+              <li className="w-full bg-[#ffffff] shadow-xl rounded-lg px-5 py-4">3 Asian Countries</li>
+              <li className="w-full bg-[#ffffff] shadow-xl rounded-lg px-5 py-4">USA and UK</li>
+            </ul>
+          </div>
+
           <div className="mt-6">
-            <div className="bg-[#F8B135] p-6  rounded-lg">
-              <div className="flex items-center space-x-12">
-                <div>
-                  <Image src={group} alt="stats" />
-                </div>
-                <div>
-                  <p className="text-[20px] md:text-[48px] font-[500], lg:text-[48px] leading-[24.5px] md:leading-[52.8px] font-[500] lg:leading-[52.8px]">
-                    Total Attendance
-                  </p>
-                  <p className="text-[20px] md:text-[48px] font-[500], lg:text-[48px] leading-[24.5px] md:leading-[52.8px] font-[700] lg:leading-[52.8px]">
-                    2447
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="mt-6">
-              <h2 className="text-[#23242A] text-[36px] leading-[39px] font-[700]">
-                Attendance Breakdown -{" "}
-              </h2>
-              <ul className="text-[#23242A] text-[32px] leading-[35px]">
-                <li>
-                  <span>Day 1:</span> States in Nigeria
-                </li>
-                <li>
-                  <span>Day 2:</span> African Countries
-                </li>
-                <li>
-                  <span>Day 3:</span> European Countries
-                </li>
-              </ul>
-            </div>
+            <h2 className="text-[#23242A] text-[36px] leading-[39px] font-[700]">
+              Attendance Breakdown -{" "}
+            </h2>
+            <ul className="text-[#23242A] text-[32px] leading-[35px]">
+              <li>
+                <span>Day 1:</span> States in Nigeria
+              </li>
+              <li>
+                <span>Day 2:</span> African Countries
+              </li>
+              <li>
+                <span>Day 3:</span> European Countries
+              </li>
+            </ul>
           </div>
         </div>
 
@@ -85,10 +106,10 @@ function Insight() {
                   <Image src={speech} alt="stats" />
                 </div>
                 <div>
-                  <p className=" text-[20px] md:text-[48px] font-[500]  leading-[24.5px] md:leading-[52.8px] lg:text-[48px] text-white lg:leading-[52.8px]">
+                  <p className="text-[20px] md:text-[48px] font-[500] leading-[24.5px] md:leading-[52.8px] lg:text-[48px] text-white lg:leading-[52.8px]">
                     Speakers:
                   </p>
-                  <p className="text-[46px] md:text-[48px] font-[500],  leading-[59.5px] md:leading-[52.8px] lg:text-[64px] text-white lg:leading-[70.4px] font-[700]">
+                  <p className="text-[46px] md:text-[48px] leading-[59.5px] md:leading-[52.8px] lg:text-[64px] text-white lg:leading-[70.4px] font-[700]">
                     50
                   </p>
                 </div>
@@ -112,10 +133,10 @@ function Insight() {
                   <Image src={play} alt="stats" />{" "}
                 </div>
                 <div>
-                  <p className=" text-[20px] md:text-[48px] font-[500]  leading-[24.5px] md:leading-[52.8px] lg:text-[48px] text-white lg:leading-[52.8px]">
+                  <p className="text-[20px] md:text-[48px] font-[500] leading-[24.5px] md:leading-[52.8px] lg:text-[48px] text-white lg:leading-[52.8px]">
                     Total Streams
                   </p>
-                  <p className="text-[46px] md:text-[48px] font-[500],  leading-[59.5px] md:leading-[52.8px] lg:text-[64px] text-white lg:leading-[70.4px] font-[700]">
+                  <p className="text-[46px] md:text-[48px] leading-[59.5px] md:leading-[52.8px] lg:text-[64px] text-white lg:leading-[70.4px] font-[700]">
                     3,161
                   </p>
                 </div>
@@ -182,6 +203,6 @@ function Insight() {
       </div>
     </div>
   );
-}
+};
 
 export default Insight;
