cf7b975a7de980010fe39572b402fc42a4aa9d3a
Nomolos29 <86138279+Nomolos29@users.noreply.github.com>
2024-07-10 00:09:09 +0100

    Updates Header

--- frontend/components/headerstats/index.tsx
+++ frontend/components/headerstats/index.tsx
@@ -13,6 +13,10 @@ function Header() {
     setMenuOpen(!menuOpen);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className="fixed top-0 left-0 right-0 bg-[#fdfefff6] shadow-md z-10 py-2  border-[2px]">
       <nav className="flex items-center justify-between w-full px-4 lg:max-w-screen-lg xl:max-w-screen-xl mx-auto md:px-6">
@@ -25,14 +29,14 @@ function Header() {
         </div>
 
         {/* Navigation links */}
-        <ul
+        <ul onClick={closeMenu}
           className={`${
             menuOpen
-              ? "flex flex-col space-y-6 leading-3 flex-4 h-screen absolute -left-1 top-20 bg-[#fff] py-3 "
+              ? "flex flex-col space-y-4 leading-3 flex-4 h-screen absolute -left-1 top-16 bg-[#8ba6ff] py-3 "
               : "md:justify-between md:space-x-1 hidden lg:flex lg:w-[30%] lg:justify-between"
           } `}>
           {headerLink.map((item) => (
-            <Link href={item.route} key={item.name}>
+            <Link href={item.route}  key={item.name}>
               <li
                 className={`text-[20px] leading-[24px hover:text-[#0D2033] p-5 lg:px-0 lg:py-2 rounded-md ${
                   route.pathname === item.route
@@ -43,6 +47,16 @@ function Header() {
               </li>
             </Link>
           ))}
+            <Link href="/apply/speaker">
+              <li
+                className={`text-[20px] leading-[24px hover:text-[#0D2033]  flex lg:hidden p-5 lg:px-0 lg:py-2 rounded-md ${
+                  route.pathname === "/apply/speaker"
+                    ? "font-semibold text-[#0D2033]"
+                    : "font-normal"
+                }`}>
+                Speaker Registration
+              </li>
+            </Link>
         </ul>
         <div className="md:w-[55%] lg:w-[43%] xl:w-[33%] md:justify-between  flex justify-end">
           {/* Nav Buttons */}
