ad572e0946f1898f98fa182715307d2d5cbaed19
Ijay_dev <inyangasi4@gmail.com>
2024-07-20 19:15:26 +0000

    feat: Add ProjectViewSet for CRUD operations on projects

--- backend/hackathon/views.py
+++ backend/hackathon/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, permissions, status
 from django.core.mail import send_mail
 from django.conf import settings
 from .serializers import TeamSerializer
-from .models import Team
+from .models import Team, Project
 from users.models import CustomUser
 
 class TeamViewSet(viewsets.ModelViewSet):
@@ -56,3 +56,53 @@ class TeamViewSet(viewsets.ModelViewSet):
         message = f'Thank you for creating the team, {instance.creator.first_name}. Your team joining code is {instance.joining_code}.'
         recipient_list = [instance.creator.email]
         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list)
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def view(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        team = Team.objects.get(id=request.data.get('team'))
+        if request.user != team.creator:
+            return Response({"error": "Only the team creator can create a project for the team."}, status=status.HTTP_400_BAD_REQUEST)
+
+        project = serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+
+        team = Team.objects.get(id=request.data.get('team'))
+        if request.user != team.creator:
+            return Response({"error": "Only the team creator can update the project for the team."}, status=status.HTTP_400_BAD_REQUEST)
+
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        team = instance.team
+
+        if request.user != team.creator:
+            return Response({"error": "Only the team creator can delete the project for the team."}, status=status.HTTP_400_BAD_REQUEST)
+
+        self.perform_destroy(instance)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
