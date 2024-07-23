b475edad8be858f506ebb451bafd5f167993e559
feranmiba <amiolademilade@gmail.com>
2024-07-23 01:55:13 +0100

    schedule-style

--- frontend/views/hackathon/dashbord/Schedule.tsx
+++ frontend/views/hackathon/dashbord/Schedule.tsx
@@ -36,7 +36,7 @@ const Schedule: React.FC = () => {
             <h1 className='text-3xl font-bold mt-5'>Event Schedule</h1>
         </section>
 
-        <section className='text-[#FFFF] flex flex-wrap mt-10'>
+        <section className='text-[#FFFF] flex flex-wrap mt-10 '>
             <div className='w-[100%] sm:w-[33.3%] md:w-[25%] bg-[#0096FF]  border-r-2 border-b-2 flex flex-col gap-2 text-start pl-3 pr-10 pt-10 pb-20 '>
                 <h3 className='bg-[#1ACF2C] w-[80px] rounded-xl pl-2'>11am WAT</h3>
                 <p>15th July, 2024</p>
