cfb541d1db669d3415b1ed7dd42a96bd14d538e9
Nomolos29 <86138279+Nomolos29@users.noreply.github.com>
2024-07-24 16:40:44 +0100

    Fixed Bugs

--- frontend/pages/hackathon/index.tsx
+++ frontend/pages/hackathon/index.tsx
@@ -2,8 +2,9 @@ import HackathonLogin from "@/views/hackathon/Login"
 
 const page = () => {
   return (
-    <div>
-        <HackathonLogin />
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1 className="text-5xl">Coming Soon</h1>
+        {/* <HackathonLogin /> */}
     </div>
   )
 }
