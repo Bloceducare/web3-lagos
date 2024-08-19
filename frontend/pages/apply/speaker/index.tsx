a66495e33a81753cbee8f80005dd2cab7fb3a25d
superman <goodnesskolapo@gmail.com>
2024-08-19 15:50:46 +0100

    closed speaker reg

--- frontend/pages/apply/speaker/index.tsx
+++ frontend/pages/apply/speaker/index.tsx
@@ -6,7 +6,9 @@ const Speaker = () => {
     <>
     <Layout>
 
-      <ApplyAsaSpeaker />
+      {/* // <ApplyAsaSpeaker /> */}
+
+      <h3 className="text-center text-[#ff0000]  text-[2em]"> Registraton Closed</h3>
     </Layout>
     </>
   );
