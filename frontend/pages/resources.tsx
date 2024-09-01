6af66220545e3434cad82d0722d622d171e09101
feranmiba <amiolademilade@gmail.com>
2024-09-01 21:47:41 +0100

    changes

--- frontend/pages/resources.tsx
+++ frontend/pages/resources.tsx
@@ -85,7 +85,7 @@ function resources() {
 
                 <section className='flex gap-10 flex-wrap lg:flex-nowrap'>
                     {Articles.map((art) => (
-                        <div className='bg-[#188BE0] text-white h-auto lg:w-[425px] self-start space-y-6 px-10 py-10 rounded-2xl'>
+                        <div className='bg-[#188BE0] text-white h-auto lg:w-[425px] self-start space-y-6 px-10 py-10 rounded-2xl' key={art.id}>
                             <h1 className='font-semibold text-3xl'>{art.title}</h1>
                             <p className='text-base leading-7 font-normal'>{art.summ}</p>
                             <button className='border px-5 py-3 text-lg rounded-xl bg-[#F8F9FB] text-[#188BE0]'>Read more</button>
@@ -133,9 +133,10 @@ function resources() {
                                 className="outline-none text-black bg-transparent flex-1 mb-3 lg:mb-0"
                                 placeholder="Enter your email"
                             />
-                            <button className="bg-[#188BE0] px-5 py-2 rounded-lg text-white">
-                                Let's go
-                            </button>
+                           <button className="bg-[#188BE0] px-5 py-2 rounded-lg text-white">
+                                Let&#39;s go
+                           </button>
+
                         </p>
                     </div>
 
