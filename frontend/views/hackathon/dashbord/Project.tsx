3421d03956d23a7a4efb8fcd576a39576098832d
superman <goodnesskolapo@gmail.com>
2024-09-05 10:59:07 +0100

    updated

--- frontend/views/hackathon/dashbord/Project.tsx
+++ frontend/views/hackathon/dashbord/Project.tsx
@@ -310,98 +310,98 @@ const Project: React.FC = () => {
         )}
 
         {teamData && teamData.projects.length === 0 && (
-          <form onSubmit={handleSubmit} className='mb-8'>
-            <div className='w-full mt-7'>
-              <label>Project</label>
-              <input
-                type="text"
-                name="name"
-                onChange={handleChange}
-                placeholder="E.g Smart Contract"
-                className="w-full p-4 border border-[#1E1E1E] shadow-[4px_4px_0px_0px_[#1E1E1E]] mt-3"
-                value={formData.name}
-                required
-              />
-              {errors.name && <p className="text-[#dc3545]">{errors.name.join(", ")}</p>}
-            </div>
-            <div className='w-full mt-7'>
-              <label>Category</label>
-              <select name="category" onChange={handleChange} value={formData.category} className="w-full p-4 border appearance-none border-[#1E1E1E] shadow-[4px_4px_0px_0px_#1E1E1E] mt-3">
-                <option value="">Select A category</option>
-                <option value="Open Governance in the Africa Electoral Process">Application in open Governance in the Africa Electoral Process</option>
-                <option value="Entertainment and media">Application in Entertainment and media</option>
-                <option value="Real World Assets">Real World Assets</option>
-                <option value="Digital collectibles">Application in digital collectibles</option>
-                <option value="Financial inclusion and education">Application in financial inclusion and education</option>
-                <option value="Sustainability">Application in sustainability</option>
-                <option value="E-Identity">E - Identity</option>
-              </select>
-              {errors.category && <p className="text-[#dc3545]">{errors.category.join(", ")}</p>}
-            </div>
-            <div className='w-full mt-7'>
-              <label>Description</label>
-              <input
-                type="text"
-                name="description"
-                onChange={handleChange}
-                placeholder="Description of your project"
-                className="w-full p-4 border border-[#1E1E1E] shadow-[4px_4px_0px_0px_#1E1E1E] mt-3"
-                value={formData.description}
-                required
-              />
-              {errors.description && <p className="text-[#dc3545]">{errors.description.join(", ")}</p>}
-            </div>
-            <div className='w-full mt-7'>
-              <label>Live Link</label>
-              <input
-                type="text"
-                name="live_link"
-                onChange={handleChange}
-                placeholder="https://web3bridge.com"
-                className="w-full p-4 border border-[#1E1E1E] shadow-[4px_4px_0px_0px_#1E1E1E] mt-3"
-                value={formData.live_link}
-                required
-              />
-              {errors.live_link && <p className="text-[#dc3545]">{errors.live_link.join(", ")}</p>}
-            </div>
-            <div className='w-full mt-7'>
-              <label>Demo video</label>
-              <input
-                type="text"
-                name="demo_video"
-                onChange={handleChange}
-                placeholder="https://www.youtube.com"
-                className="w-full p-4 border border-[#1E1E1E] shadow-[4px_4px_0px_0px_#1E1E1E] mt-3"
-                value={formData.demo_video}
-                required
-              />
-              {errors.demo_video && <p className="text-[#dc3545]">{errors.demo_video.join(", ")}</p>}
-            </div>
-            <div className='w-full mt-7'>
-              <label>GitHub Link</label>
-              <input
-                type="text"
-                name="github_url"
-                onChange={handleChange}
-                placeholder="https://github.com/username/repository"
-                className="w-full p-4 border border-[#1E1E1E] shadow-[4px_4px_0px_0px_#1E1E1E] mt-3"
-                value={formData.github_url}
-                required
-              />
-              {errors.github_url && <p className="text-[#dc3545]">{errors.github_url.join(", ")}</p>}
-            </div>
-            <div className='w-full mt-7'>
-              <button
-                type="submit"
-                className="w-full mt-12 py-4 bg-[#1E1E1E] text-white text-xl text-center shadow-[-5px_-5px_0px_0px_#0096FF]"
-                disabled={loading}
-              >
-                {loading ? "Loading..." : "Submit"}
-              </button>
-            </div>
-          </form>
+          // <form onSubmit={handleSubmit} className='mb-8'>
+          //   <div className='w-full mt-7'>
+          //     <label>Project</label>
+          //     <input
+          //       type="text"
+          //       name="name"
+          //       onChange={handleChange}
+          //       placeholder="E.g Smart Contract"
+          //       className="w-full p-4 border border-[#1E1E1E] shadow-[4px_4px_0px_0px_[#1E1E1E]] mt-3"
+          //       value={formData.name}
+          //       required
+          //     />
+          //     {errors.name && <p className="text-[#dc3545]">{errors.name.join(", ")}</p>}
+          //   </div>
+          //   <div className='w-full mt-7'>
+          //     <label>Category</label>
+          //     <select name="category" onChange={handleChange} value={formData.category} className="w-full p-4 border appearance-none border-[#1E1E1E] shadow-[4px_4px_0px_0px_#1E1E1E] mt-3">
+          //       <option value="">Select A category</option>
+          //       <option value="Open Governance in the Africa Electoral Process">Application in open Governance in the Africa Electoral Process</option>
+          //       <option value="Entertainment and media">Application in Entertainment and media</option>
+          //       <option value="Real World Assets">Real World Assets</option>
+          //       <option value="Digital collectibles">Application in digital collectibles</option>
+          //       <option value="Financial inclusion and education">Application in financial inclusion and education</option>
+          //       <option value="Sustainability">Application in sustainability</option>
+          //       <option value="E-Identity">E - Identity</option>
+          //     </select>
+          //     {errors.category && <p className="text-[#dc3545]">{errors.category.join(", ")}</p>}
+          //   </div>
+          //   <div className='w-full mt-7'>
+          //     <label>Description</label>
+          //     <input
+          //       type="text"
+          //       name="description"
+          //       onChange={handleChange}
+          //       placeholder="Description of your project"
+          //       className="w-full p-4 border border-[#1E1E1E] shadow-[4px_4px_0px_0px_#1E1E1E] mt-3"
+          //       value={formData.description}
+          //       required
+          //     />
+          //     {errors.description && <p className="text-[#dc3545]">{errors.description.join(", ")}</p>}
+          //   </div>
+          //   <div className='w-full mt-7'>
+          //     <label>Live Link</label>
+          //     <input
+          //       type="text"
+          //       name="live_link"
+          //       onChange={handleChange}
+          //       placeholder="https://web3bridge.com"
+          //       className="w-full p-4 border border-[#1E1E1E] shadow-[4px_4px_0px_0px_#1E1E1E] mt-3"
+          //       value={formData.live_link}
+          //       required
+          //     />
+          //     {errors.live_link && <p className="text-[#dc3545]">{errors.live_link.join(", ")}</p>}
+          //   </div>
+          //   <div className='w-full mt-7'>
+          //     <label>Demo video</label>
+          //     <input
+          //       type="text"
+          //       name="demo_video"
+          //       onChange={handleChange}
+          //       placeholder="https://www.youtube.com"
+          //       className="w-full p-4 border border-[#1E1E1E] shadow-[4px_4px_0px_0px_#1E1E1E] mt-3"
+          //       value={formData.demo_video}
+          //       required
+          //     />
+          //     {errors.demo_video && <p className="text-[#dc3545]">{errors.demo_video.join(", ")}</p>}
+          //   </div>
+          //   <div className='w-full mt-7'>
+          //     <label>GitHub Link</label>
+          //     <input
+          //       type="text"
+          //       name="github_url"
+          //       onChange={handleChange}
+          //       placeholder="https://github.com/username/repository"
+          //       className="w-full p-4 border border-[#1E1E1E] shadow-[4px_4px_0px_0px_#1E1E1E] mt-3"
+          //       value={formData.github_url}
+          //       required
+          //     />
+          //     {errors.github_url && <p className="text-[#dc3545]">{errors.github_url.join(", ")}</p>}
+          //   </div>
+          //   <div className='w-full mt-7'>
+          //     <button
+          //       type="submit"
+          //       className="w-full mt-12 py-4 bg-[#1E1E1E] text-white text-xl text-center shadow-[-5px_-5px_0px_0px_#0096FF]"
+          //       disabled={loading}
+          //     >
+          //       {loading ? "Loading..." : "Submit"}
+          //     </button>
+          //   </div>
+          // </form>
 
-          // <div className="text-[#ff0000] text-[1.5em] m-auto">Submission Closed</div>
+          <div className="text-[#ff0000] text-[1.5em] m-auto">Submission Closed</div>
         )}
 
         {teamData && teamData.projects.length > 0 && (
