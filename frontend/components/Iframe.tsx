9ccaa766a37b36a79657d0c8ab9069ac923b18ac
favydabs <favourdabit@gmail.com>
2025-02-12 20:09:25 +0100

    done with landing page

--- frontend/components/Iframe.tsx
+++ frontend/components/Iframe.tsx
@@ -19,22 +19,14 @@ const YoutubeIframe: React.FC<YoutubeIframeProps> = ({ children }) => {
   };
 
   return (
-    <section className="flex flex-col items-center justify-between bg-hero space-y-6 w-full py-[3rem] md:py-[3rem]">
-      <div className="p-4 lg:max-w-screen-lg xl:max-w-screen-xl">
-        <div className="space-y-[3rem]">
-          <h3 className="text-center font-bold my-4 text-white text-3xl lg:text-4xl leading-10 lg:leading-12">
-            Highlights from W3LC 2023
-          </h3>
-          <div className="aspect-w-6 aspect-h-9">
-            <YouTube
-              videoId="EbcGAXOTWbA"
-              opts={opts}
-              onReady={onPlayerReady}
-            />
-          </div>
+    <section className="flex flex-col items-center justify-center w-full py-12">
+      <div className="w-full max-w-2xl p-4 text-center">
+        
+        <div className="aspect-w-16 aspect-h-9">
+          <YouTube videoId="EbcGAXOTWbA" opts={opts} onReady={onPlayerReady} />
         </div>
         {children && (
-          <p className="text-center mt-[3rem] text-lg  lg:leading-[1.7em] lg:text-[24px] px-4 lg:px-0 text-white">
+          <p className="mt-6 text-lg lg:leading-[1.7em] lg:text-[24px] px-4 text-white">
             {children}
           </p>
         )}
